b1d44a5da21f42ec07afde1db82c97ed3455b103
Geovana kidwell <pradogeovana07@gmail.com>
2024-07-18 09:55:01 -0400

    third commit

---
+++ week2/test2.py
@@ -0,0 +1,23 @@
+# 1. Using a For Loop, write a program that loops through and prints your first name.
+# hint: for i in "Myblog":
+#               print (i, '?')
+
+
+
+#2. Using a For Loop, write a program that loops through the name ANONYMOUS and prints
+
+#3. Using a while loop, create an infinite loop. (Remember to stop the program after you run it for a few seconds)
+
+#4. Write a function named area that accepts the base and perpendicular of a right-angled triangle as two arguments and returns the area of that triangle.
+
+#5. Write a function named times_ten. The function should accept an argument and display the product of its argument multiplied times 10.
+
+#6. Write a function named welcome() that asks the user to enter his or her name and displays it followed by a welcome message.
+
+#7. Write a program that gets input from the user and writes that input into a file that you will call ‘friends.txt’.
+
+#8. Write a program that will read ‘ip.txt’ as soon as the boss executes it.
+
+#9. Write a program that will write 3 IP Addresses (192.168.2.3, 10.0.0.1, 192.168.44.3) into a file named ‘ip.txt’. The boss needs them!! 
+
+#10. Write a program that will write THREE NAMES into a text file names ‘philosophers.txt’.
